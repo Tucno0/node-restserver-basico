d92791f6c2d7286463e2d7b05619211d259b5d7f
Jhampier <yantucnoperez@gmail.com>
2023-09-29 23:10:37 -0500

    PORT added and npm script start added

--- models/server.js
+++ models/server.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 class Server {
   constructor() {
     this.app = express()
-    this.port = process.env.PORT
+    this.port = process.env.PORT ?? 3000
     this.usuariosPath = '/api/usuarios'
 
     /**
@@ -41,7 +41,7 @@ class Server {
   routes() {
     this.app.get('/', (req, res) => {
       res.json({
-        msg: 'get API',
+        msg: 'get API'
       })
     })
 
